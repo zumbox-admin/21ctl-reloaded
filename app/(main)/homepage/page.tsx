017c2bd5464d65971fe28c6589efc207fefedc92
Cypher012 <ayoojoade@gmail.com>
2025-02-01 01:14:01 +0100

    feat: refactor homepage component by moving constants to a separate file for better organization

--- app/(main)/homepage/page.tsx
+++ app/(main)/homepage/page.tsx
@@ -7,49 +7,8 @@ import SuccessStories from "@/app/(main)/homepage/SuccessStories";
 import { cn } from "@/lib/utils";
 import Image from "next/image"
 import LatestUpdate from "./LatestUpdate";
+import {buttonData,featuresIcons,paternershipIcons,stats} from "@/constants/homepage"
 
-const buttonData = [
-  { cta: "Data Center", className: "bg-[#FFFAC5]" },
-  { cta: "Konet", className: "bg-cerise-lighter" },
-  { cta: "Payment", className: "bg-curiousBlue" },
-  { cta: "Power", className: "bg-[#F2FFC8]" },
-  { cta: "Academy", className: "bg-[#FFFAC5]" },
-];
-
-const stats = [
-  { value: "20K", label: "company stat 1" },
-  { value: "15K", label: "company stat 2" },
-  { value: "10K", label: "company stat 3" },
-  { value: "5K", label: "company stat 4" },
-  { value: "2.5K", label: "company stat 5" },
-  { value: "1K", label: "company stat 6" },
-];
-
-const featuresIcons = [
-  {title: "Telecome", text: "Enhancing connectivity and communication networks.", icons: "./icons/connect.svg", bgColor:"bg-cerise-lighter"},
-  {title: "Finance", text: "Secure, reliable tools for digital transactions.", icons: "./icons/credit-card-validation.svg", bgColor:"bg-curiousBlue"},
-  {title: "Education", text: "Empowering institutions with advanced learning platforms.", icons: "./icons/database-setting.svg", bgColor:"bg-dimYellow"},
-  {title: "Energy", text: "Driving sustainability with innovative power solutions.", icons: "./icons/power-service.svg", bgColor:"bg-dimYellow"},
-
-]
-
-
-const paternershipIcons = [ './paternership-logos/BHARTIARTL.NS-de86ce43 1 (copy).svg',
-'./paternership-logos/BHARTIARTL.NS-de86ce43 1.svg',
-'./paternership-logos/GPJwKp-WoAAm8A1 1 (copy).svg',
-'./paternership-logos/GPJwKp-WoAAm8A1 1.svg',
-'./paternership-logos/icons8-amazon-web-services 1 (copy).svg',
-'./paternership-logos/icons8-amazon-web-services 1.svg',
-'./paternership-logos/oracle_hjzrzq 1 (copy).svg',
-'./paternership-logos/oracle_hjzrzq 1.svg',
-'./paternership-logos/orange_ymolsp 2 (copy).svg',
-'./paternership-logos/orange_ymolsp 2.svg',
-'./paternership-logos/Schneider-Electric-logo 1 (copy).svg',
-'./paternership-logos/Schneider-Electric-logo 1.svg',
-'./paternership-logos/super-micro 1 (copy).svg',
-'./paternership-logos/super-micro 1.svg',
-'./paternership-logos/vmware-logo-black 1 (copy).svg',
-'./paternership-logos/vmware-logo-black 1.svg',]
 
 const HomePage = () => {
   return (
---
+++ constants/homepage.ts
@@ -0,0 +1,42 @@
+export const buttonData = [
+    { cta: "Data Center", className: "bg-[#FFFAC5]" },
+    { cta: "Konet", className: "bg-cerise-lighter" },
+    { cta: "Payment", className: "bg-curiousBlue" },
+    { cta: "Power", className: "bg-[#F2FFC8]" },
+    { cta: "Academy", className: "bg-[#FFFAC5]" },
+  ];
+  
+export const stats = [
+    { value: "20K", label: "company stat 1" },
+    { value: "15K", label: "company stat 2" },
+    { value: "10K", label: "company stat 3" },
+    { value: "5K", label: "company stat 4" },
+    { value: "2.5K", label: "company stat 5" },
+    { value: "1K", label: "company stat 6" },
+  ];
+  
+export const featuresIcons = [
+    {title: "Telecome", text: "Enhancing connectivity and communication networks.", icons: "./icons/connect.svg", bgColor:"bg-cerise-lighter"},
+    {title: "Finance", text: "Secure, reliable tools for digital transactions.", icons: "./icons/credit-card-validation.svg", bgColor:"bg-curiousBlue"},
+    {title: "Education", text: "Empowering institutions with advanced learning platforms.", icons: "./icons/database-setting.svg", bgColor:"bg-dimYellow"},
+    {title: "Energy", text: "Driving sustainability with innovative power solutions.", icons: "./icons/power-service.svg", bgColor:"bg-dimYellow"},
+  
+  ]
+  
+  
+export const paternershipIcons = [ './paternership-logos/BHARTIARTL.NS-de86ce43 1 (copy).svg',
+  './paternership-logos/BHARTIARTL.NS-de86ce43 1.svg',
+  './paternership-logos/GPJwKp-WoAAm8A1 1 (copy).svg',
+  './paternership-logos/GPJwKp-WoAAm8A1 1.svg',
+  './paternership-logos/icons8-amazon-web-services 1 (copy).svg',
+  './paternership-logos/icons8-amazon-web-services 1.svg',
+  './paternership-logos/oracle_hjzrzq 1 (copy).svg',
+  './paternership-logos/oracle_hjzrzq 1.svg',
+  './paternership-logos/orange_ymolsp 2 (copy).svg',
+  './paternership-logos/orange_ymolsp 2.svg',
+  './paternership-logos/Schneider-Electric-logo 1 (copy).svg',
+  './paternership-logos/Schneider-Electric-logo 1.svg',
+  './paternership-logos/super-micro 1 (copy).svg',
+  './paternership-logos/super-micro 1.svg',
+  './paternership-logos/vmware-logo-black 1 (copy).svg',
+  './paternership-logos/vmware-logo-black 1.svg',]
